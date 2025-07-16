9c875fa584082630209258a1706c6a2248e2f305
Liamf <liam.fanenbruck@gmail.com>
2025-07-16 17:37:27 +0200

    v1

---
+++ src/main/java/de/HiorCraft/smp/essental_SMP.java
@@ -0,0 +1,17 @@
+package de.HiorCraft.smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class essental_SMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
