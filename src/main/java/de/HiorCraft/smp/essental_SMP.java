a8acd7ebce931442cd0467ec0d5f2e6a9eb35cfc
Liamf <liam.fanenbruck@gmail.com>
2025-07-23 20:49:44 +0200

    add Double jump

--- src/main/java/de/HiorCraft/smp/essental_SMP.java
+++ src/main/java/de/HiorCraft/smp/essental_SMP.java
@@ -7,6 +7,8 @@ public final class essental_SMP extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Essential SMP Plugin is starting...");
+        getServer().getPluginManager().registerEvents(new de.HiorCraft.smp.duble_jump.double_jump(), this);
+        getLogger().info("Essential SMP Plugin is starting...");
 
     }
 
